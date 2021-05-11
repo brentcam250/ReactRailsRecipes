18e143b31299d1279d80ba3c8b03c5d6ac70fecf
Brent Cameron <bjcameron@gmail.com>
2021-05-11 13:26:50 -0600

    added controller for recipes, filled in methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
+  # get 'homepage/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
 end
